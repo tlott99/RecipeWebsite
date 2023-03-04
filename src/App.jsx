c1de06fed7971c84eaf27b9563019e96001466a8
Kenny Jacobson <>
2023-03-04 21:37:24 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Experiment from './Experiment'
+
+export default function App() {
+  return (
+
+      <Experiment />
+    
+  )
+}
---
+++ src/Experiment.jsx
@@ -0,0 +1,15 @@
+
+// const Database = require("@replit/database")
+export default function Experiment() {
+  
+  
+  // const db = new Database()
+  
+  // db.set("recipe1", "{'name':'Spaghetti'}").then(() => {});
+  
+  return (
+    <div>
+      I am the experiment more stuff.
+    </div>
+  )
+}
