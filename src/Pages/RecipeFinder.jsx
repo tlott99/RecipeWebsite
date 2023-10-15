b415dc7cbce8293d4897680cf5173f0bbf1287e5
tlott99 <70763259+tlott99@users.noreply.github.com>
2023-10-14 21:01:35 -0400

    Recipe Finder is searchable now

--- src/Pages/RecipeFinder.jsx
+++ src/Pages/RecipeFinder.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'graphql-tag';
-import { Typography, Box, Container, Button, TextField } from '@mui/material';
+import { Typography, Box, Container, Button, TextField, FormControl, InputLabel, Select, MenuItem} from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 
 const getRecipes = gql`
@@ -21,19 +21,53 @@ query Recipes{
 export default function RecipeFinder() {
   const { loading, error, data } = useQuery(getRecipes);
   const [searchTerm, setSearchTerm] = useState('');
+  const [searchCategory, setSearchCategory] = useState('title');
+  const [typeSearch, setTypeSearch] = useState('Breakfast'); 
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(  </p>;
   const title="All Recipes";
- 
+  
   const handleSearch = (e) => {
-    setSearchTerm(e.target.value);
-    // You can perform search-related logic here
+    const search = e.target.value.toLowerCase();
+    setSearchTerm(search);
   };
+
+  // const recipesToDisplay = data.recipes.filter((recipe) =>
+  //   recipe[searchCategory].toLowerCase().includes(searchTerm.toLowerCase())
+  // );
+  const recipesToDisplay = data.recipes.filter((recipe) => {
+    const searchTermLower = searchTerm.toLowerCase();
+  
+    if (searchCategory === 'mealType') {
+      const mealTypeData = JSON.parse(recipe.mealType);
+      return (
+        (typeSearch === 'All' || mealTypeData.checkedMeal.toLowerCase() === typeSearch.toLowerCase()) &&
+        recipe.title.toLowerCase().includes(searchTermLower)
+      );
+    } else {
+      const fieldToCheck = recipe[searchCategory].toLowerCase();
+      return fieldToCheck.includes(searchTermLower);
+    }
+  });
+  
 
   return (
     <Container >  
       <Typography variant="h2" sx={{mb:3}}>{title}</Typography>
+      <FormControl sx={{ mb: 3 }}>
+        <InputLabel htmlFor="search-category">Search By</InputLabel>
+        <Select
+          value={searchCategory}
+          onChange={(e) => setSearchCategory(e.target.value)}
+          inputProps={{ name: 'searchCategory', id: 'search-category' }}
+        >
+          <MenuItem value="title">Title</MenuItem>
+          <MenuItem value="mealType">Meal Type</MenuItem>
+          {/* Add more search categories as needed */}
+        </Select>
+      </FormControl>
+      
       <TextField 
        label={
         <>
@@ -46,8 +80,26 @@ export default function RecipeFinder() {
       onChange={handleSearch}
       sx={{width:"50%", mb:3}}
     />
+      
+      { searchCategory == 'mealType' && (
+        <FormControl sx={{ mb: 3 }}>
+          <InputLabel htmlFor="search-category">Meal Type</InputLabel>
+          <Select
+          value={typeSearch}
+          onChange={(e) => setTypeSearch(e.target.value)}
+          inputProps={{ name: 'searchCategory', id: 'search-category' }}
+          >
+            <MenuItem value="Breakfast">Breakfast</MenuItem>
+            <MenuItem value="Lunch">Lunch</MenuItem>
+            <MenuItem value="Dinner">Dinner</MenuItem>
+            <MenuItem value="Dessert">Dessert</MenuItem>
+            <MenuItem value="Drink">Drink</MenuItem>
+            {/* Add more search categories as needed */}
+          </Select>
+        </FormControl>
+      )}
       {
-        data.recipes
+        recipesToDisplay
           .slice()
           .sort((a, b) => a.title.localeCompare(b.title))
           .map((recipe, index) => (
